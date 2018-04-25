612aaa799b03d5bf2895c3df872109c53778801a
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-04-25 21:19:06 +0500

    remove extra packages

--- app/src/web/main.js
+++ app/src/web/main.js
@@ -1,20 +1,15 @@
 import Vue from 'vue'
 import 'bulma-fluent/css/bulma.css'
 
-import vueElectron from 'vue-electron'
-
-import './mixins'
-
 import './assets/style/main.sass'
 import './assets/style/animations.sass'
 import './assets/fontawesome/css/fontawesome-all.css'
 
+import './mixins'
 import App from './App'
 import router from './router'
 import store from './store'
 import dataPaths from "../../../utilities/data-paths.js";
-
-Vue.use(vueElectron)
 
 // eslint-disable-next-line
 global.__paths = dataPaths()
