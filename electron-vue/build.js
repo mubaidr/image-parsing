09d78cfb8fa3c2a143b750450e59af94577dbd98
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-04-29 12:36:53 +0500

    fix build script

--- electron-vue/build.js
+++ electron-vue/build.js
@@ -1,92 +1,44 @@
-process.env.NODE_ENV = 'production'
-
+/* eslint-disable*/
 const del = require('del')
-const {
-  spawn
-} = require('child_process')
 const webpack = require('webpack')
-const Multispinner = require('multispinner')
+/* eslint-enable */
 
 const mainConfig = require('./webpack.main.config')
 const rendererConfig = require('./webpack.renderer.config')
 
-const doneLog = `${chalk.bgGreen.white(' DONE ')  } `
-const errorLog = `${chalk.bgRed.white(' ERROR ')  } `
-const okayLog = `${chalk.bgBlue.white(' OKAY ')  } `
-const isCI = process.env.CI || false
-
-if (process.env.BUILD_TARGET === 'clean') clean()
-else build()
-
 function clean() {
   del.sync(['dist/*', 'build/*', '!build/icons', '!build/icons/icon.*'])
-  console.log(`\n${doneLog}\n`)
+  console.log(`\nCleaned!`)
   process.exit()
+}
+
+function pack(config) {
+  return new Promise((resolve, reject) => {
+    webpack(config, (err) => {
+      if (err) reject(err.stack || err)
+
+      resolve()
+    })
+  })
 }
 
 function build() {
   del.sync(['dist/electron/*', '!.gitkeep'])
 
-  const tasks = ['main', 'renderer']
-  const m = new Multispinner(tasks, {
-    preText: 'building',
-    postText: 'process'
-  })
-
-  let results = ''
-
-  m.on('success', () => {
-    process.stdout.write('\x1B[2J\x1B[0f')
-    console.log(`\n\n${results}`)
-    console.log(
-      `${okayLog}take it away ${chalk.yellow('`electron-builder`')}\n`)
-    process.exit()
-  })
-
-  pack(mainConfig).then(result => {
-    results += `${result  }\n\n`
-    m.success('main')
+  pack(mainConfig).then(() => {
+    console.log('Compiled Main!')
   }).catch(err => {
-    m.error('main')
-    console.log(`\n  ${errorLog}failed to build main process`)
-    console.error(`\n${err}\n`)
+    console.error(`\n${err}`)
     process.exit(1)
   })
 
-  pack(rendererConfig).then(result => {
-    results += `${result  }\n\n`
-    m.success('renderer')
+  pack(rendererConfig).then(() => {
+    console.log('Compiled Renderer!')
   }).catch(err => {
-    m.error('renderer')
-    console.log(`\n  ${errorLog}failed to build renderer process`)
-    console.error(`\n${err}\n`)
+    console.error(`\n${err}`)
     process.exit(1)
   })
 }
 
-function pack(config) {
-  return new Promise((resolve, reject) => {
-    webpack(config, (err, stats) => {
-      if (err) reject(err.stack || err)
-      else if (stats.hasErrors()) {
-        let err = ''
-
-        stats.toString({
-            chunks: false,
-            colors: true
-          })
-          .split(/\r?\n/)
-          .forEach(line => {
-            err += `    ${line}\n`
-          })
-
-        reject(err)
-      } else {
-        resolve(stats.toString({
-          chunks: false,
-          colors: true
-        }))
-      }
-    })
-  })
-}
+if (process.env.BUILD_TARGET === 'clean') clean()
+else build()
