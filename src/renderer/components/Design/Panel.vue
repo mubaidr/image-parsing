24470f6b68c31ca56f761bd45f7dd9a4b869720c
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-05-01 11:05:33 +0500

    prepare to implement designer

--- src/renderer/components/Design/Panel.vue
+++ src/renderer/components/Design/Panel.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="box">
     <h1>Panel</h1>
+    <canvas ref="myCanvas"></canvas>
   </div>
 </template>
 
 <script>
-// import tool from './Panel/tool'
+import tool from './Panel/tool'
 
 export default {
   mounted() {
-    // tool.init()
+    tool.init(this.$refs.myCanvas)
   }
 }
 </script>
