38a9fe3a7a606c4095ea7df707eb3792fbb3767d
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-06-07 13:43:48 +0500

    [WIP] process script multithreading

--- src/utilities/process.js
+++ src/utilities/process.js
@@ -1,3 +1,5 @@
+const os = require('os')
+
 const {
   getDesignData,
   getImagePaths,
@@ -24,27 +26,22 @@ async function processTask(designData, imagePaths, neuralNet) {
         Object.keys(pre).forEach((key, index) => {
           resultArray[index] = { key, val: pre[key] }
         })
-
         resultArray.sort((a, b) => b.val - a.val)
-        let value = resultArray[0]
 
-        if (value.val >= 0.95 && value.key === '?') {
-          resultsJson[rollNo][q.title] = value.key
+        let topKeyValue = resultArray[0]
+
+        if (topKeyValue.val >= 0.95 && topKeyValue.key === '?') {
+          resultsJson[rollNo][q.title] = topKeyValue.key
         } else {
-          const newArray = resultArray
-            .filter(item => item.key !== '?')
-            .sort((a, b) => b.val - a.val)
+          const newArray = resultArray.filter(item => item.key !== '?')
 
           // eslint-disable-next-line
-          value = newArray[0]
+          topKeyValue = newArray[0]
 
-          if (
-            newArray[1].val >= 0.5 ||
-            newArray[0].val - newArray[1].val < 20
-          ) {
+          if (topKeyValue.val < 0.5 || topKeyValue.val - newArray[1].val < 20) {
             resultsJson[rollNo][q.title] = '*'
           } else {
-            resultsJson[rollNo][q.title] = value.key.toUpperCase()
+            resultsJson[rollNo][q.title] = topKeyValue.key.toUpperCase()
           }
         }
       })
@@ -53,16 +50,34 @@ async function processTask(designData, imagePaths, neuralNet) {
 }
 
 async function process() {
-  const designData = await getDesignData()
-  const imagePaths = await getImagePaths()
-  const neuralNet = await getNeuralNet()
+  const [designData, imagePaths, neuralNet] = await Promise.all([
+    getDesignData(),
+    getImagePaths(),
+    getNeuralNet(),
+  ])
 
-  // const NO_OF_CPUS = os.cpus.length
+  const NO_OF_CORES = os.cpus.length * 2 // use hyper-threading
+  const TOTAL_IMAGES = imagePaths.length
+  const promises = []
+
   // const resultsJson = {}
 
-  // TODO: split images collection and divide among all cores
-  processTask(designData, imagePaths, neuralNet)
-  // TODO: collect result from each process
+  for (let i = 0; i < NO_OF_CORES; i += 1) {
+    const startIndex = Math.floor(i * (TOTAL_IMAGES / NO_OF_CORES))
+    const endIndex =
+      i < NO_OF_CORES
+        ? Math.floor((i + 1) * (TOTAL_IMAGES / NO_OF_CORES))
+        : TOTAL_IMAGES - 1
+
+    promises.push(
+      processTask(designData, imagePaths.slice(startIndex, endIndex), neuralNet)
+    )
+  }
+
+  Promise.all(promises).then(res => {
+    // DEBUG: Should contain json parts equal to NO_OF_CORES
+    console.log(res)
+  })
 
   /*
   fs.writeFileSync(
@@ -74,7 +89,7 @@ async function process() {
     `${appPaths.trainingData}\\data-output.csv`,
     jsonToCsv(resultsJson)
   )
-*/
+  */
 }
 
 module.exports = {
