e4675c029ffded29dc9517d54fd65088009f03fc
Muhammad Ubaid Raza <mubaidr@gmail.com>
2019-07-29 12:04:41 -0700

    add dev configs

--- src/utilities/_test/index.ts
+++ src/utilities/_test/index.ts
@@ -3,7 +3,7 @@ import { readFileSync } from 'fs'
 
 const str = readFileSync(
   'D:\\current\\image-parsing\\__tests__\\test-data\\design.svg'
-)
+).toString()
 
 const options = {
   attributeNamePrefix: '',
@@ -13,6 +13,6 @@ const options = {
   allowBooleanAttributes: true,
 }
 
-const json = parse(str.toString(), options)
+const json = parse(str, options)
 
 console.dir(json.svg)
