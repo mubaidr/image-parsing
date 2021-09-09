4017f3465efdec6d581fcaa775bbc9c3b9fdbf04
Muhammad Ubaid Raza <mubaidr@gmail.com>
2021-09-09 17:59:32 +0500

    fix config error

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { DefinePlugin } = require('webpack')
-const { name } = require('./package.json')
+// const { DefinePlugin } = require('webpack')
+// const { name } = require('./package.json')
 
 module.exports = {
   configureWebpack: {
     devtool: process.env.NODE_ENV === 'development' ? 'eval-source-map' : false,
     plugins: [
-      new DefinePlugin({
-        'process.env.NAME': name,
-      }),
+      // new DefinePlugin({
+      //   'process.env.NAME': name,
+      // }),
     ],
     target: 'electron-renderer',
   },
